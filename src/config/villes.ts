622c5ee5db56bf05ba70769f353438dbcf2136ff
home popy <home@iMac-de-home-001.lan>
2026-04-22 13:51:48 +0200

    Initial commit - DIAGOTOP site Astro

---
+++ src/config/villes.ts
@@ -0,0 +1,12 @@
+export const VILLES_DESSERVIES = [
+  "paris-12",
+  "paris-15",
+  "boulogne-billancourt",
+  "vincennes",
+  "versailles",
+  "nanterre",
+  "creteil",
+  "saint-denis",
+  "montreuil",
+  "argenteuil"
+];
