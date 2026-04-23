a6e0cae2cd326b33d77358844032eb6565166a60
home popy <home@iMac-de-home-001.lan>
2026-04-23 12:36:20 +0200

    Ajout du module sitemap Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://diagotop.fr',
+  site: 'https://www.diagotop.fr',
+  integrations: [sitemap()],
 });
